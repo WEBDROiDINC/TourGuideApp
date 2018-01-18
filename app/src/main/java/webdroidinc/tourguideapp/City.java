c1f1f8bb446a88c01440b0021582ca004ec8544d
WEBDROiD <info@webdroid.org>
2018-01-18 10:08:14 +0200

    City and Place Classes added

---
+++ app/src/main/java/webdroidinc/tourguideapp/City.java
@@ -0,0 +1,15 @@
+package webdroidinc.tourguideapp;
+
+import android.location.Address;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Blood_seeker on 1/18/2018.
+ */
+
+public class City {
+    private Address Location ;
+    private ArrayList<Place> CityPlaces;
+
+}
